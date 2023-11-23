2cacc5c7140085e0d3d4bc6bb8b4813343599014
Rojotiana <rojotiana.tsiverisoa@gmail.com>
2023-11-23 16:31:43 +0300

    set up TabBar

---
+++ lib/class/tabbar_menu.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TabBarMenu {
+  String title;
+  Widget content;
+
+  TabBarMenu( this.title, this.content);
+}
--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:todo_list/colors/my_colors.dart';
 import 'package:todo_list/screen/my_drawer.dart';
+import 'package:todo_list/class/tabbar_menu.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -13,18 +14,65 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(''),
-          backgroundColor: MyColors.c1,
-          iconTheme: const IconThemeData(
-            color: Colors.white,
+    List<TabBarMenu> menuList = [
+      TabBarMenu(
+        'New task',
+        const SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Add a new task'),
+              ],
+            ),
           ),
         ),
-        drawer: const MyDrawer(),
-        body: const SingleChildScrollView(
-          child: Text('Hello World'),
+      ),
+      TabBarMenu(
+        'Details',
+        const SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Details'),
+              ],
+            ),
+          ),
+        ),
+      ),
+    ];
+
+    List<Tab> myTabs = menuList
+        .map((menu) => Tab(
+              text: menu.title,
+            ))
+        .toList();
+
+    contents() {
+      return menuList.map((menu) => (menu.content)).toList();
+    }
+
+    return SafeArea(
+      child: DefaultTabController(
+        length: menuList.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text(''),
+            backgroundColor: MyColors.c1,
+            iconTheme: const IconThemeData(
+              color: Colors.white,
+            ),
+            bottom: TabBar(
+              tabs: myTabs,
+            ),
+          ),
+          drawer: const MyDrawer(),
+          body: TabBarView(
+            children: contents(),
+          ),
         ),
       ),
     );
